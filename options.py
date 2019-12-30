794ad9e4abead7fa5a7fdd5c2bff3dba881306fc
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-12-31 02:47:29 +0900

    Add mask option and arange

--- options.py
+++ options.py
@@ -13,21 +13,84 @@ def get_options(args=None):
     )
 
     # main
-    parser.add_argument('--input', type=str, required=True)
-    parser.add_argument('--mode', type=str, default='exec', choices=['exec', 'debug', 'save'])
-    parser.add_argument('--log_root', type=str, default='log')
-    parser.add_argument('--gpu_id', type=int, default=0)
+    parser.add_argument(
+        '--input',
+        type=str, required=True,
+        help='path to input file'
+    )
+    parser.add_argument(
+        '--mode', 
+        type=str, default='exec', choices=['exec', 'debug', 'save'],
+        help='mode for execution'
+    )
+    parser.add_argument(
+        '--log_root', 
+        type=str, default='log',
+        help='path to root directory for log'
+    )
+    parser.add_argument(
+        '--gpu_id', 
+        type=int, default=0,
+        help='id for cuda'
+    )
 
     # resize
-    parser.add_argument('--resize_factor', type=float, default=None)
+    parser.add_argument(
+        '--resize_factor', 
+        type=float, default=None,
+        help='factor of resizing input image'
+    )
     
     # segmentation
-    parser.add_argument('--semseg', type=str, default='DeepLabV3')
-    parser.add_argument('--resnet', type=int, default=18)
+    parser.add_argument(
+        '--semseg_method', 
+       type=str, default='DeepLabV3', choices=['DeepLabV3'],
+       help='method of semantic segmentation'
+    )
+    parser.add_argument(
+        '--resnet', 
+        type=int, default=18,
+        help='depth of backborn resnet'
+    )
+
+    # mask
+    parser.add_argument(
+        '--shadow_angle', 
+        type=int, default=30,
+        help='angle for including shadow to mask'
+    )
+    parser.add_argument(
+        '--kernel_ratio', 
+        type=float, default=0.01,
+        help='kernel size = image size * kernel_ratio'
+    )
+    parser.add_argument(
+        '--shadow_height_factor', 
+        type=int, default=5,
+        help='shadow_height = image size * kernel_ratio * shadow_height_factor'
+    )
+    parser.add_argument(
+        '--rough_obj_size', 
+        type=strtobool, default=False,
+        help='use rough obj size and skip the process for estimating max obj size for making process fast'
+    )
 
     # inpaint
-    parser.add_argument('--inpaint', type=str, default='EdgeConnect')
-    parser.add_argument('--sigma', type=int, default=2, help='sigma of canny edge detection')
+    parser.add_argument(
+        '--inpaint_method', 
+        type=str, default='EdgeConnect', choices=['EdgeConnect'],
+        help='method of image inpainting'
+    )
+    parser.add_argument(
+        '--sigma', 
+        type=int, default=2, 
+        help='sigma of canny edge detection'
+    )
+    parser.add_argument(
+        '--max_hole_size', 
+        type=int, default=128,
+        help='maximum hole size when inpainting'
+    )
 
     # parse, print, and return
     opt = parser.parse_args(args)
@@ -62,5 +125,5 @@ def _print_options(parser, opt):
             comment = '\t[default: {}]'.format(str(default))
         message += '{:>15}: {:<25}{}\n'.format(str(k), str(v), comment)
     message += '---------------------------- End ------------------------------'
-    print(message)
+    print(message, '\n')
 
