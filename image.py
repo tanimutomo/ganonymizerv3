01f3e7523dc1afc23f0b336b62ed8f1b315166b0
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-12-31 11:53:50 +0900

    Add elapsed time to image.py

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 import os
 import PIL
+import time
 
 from PIL import Image
 
@@ -11,7 +12,9 @@ def main(args=None):
     opt = get_options(args)
     img, opt.fname, opt.fext = load_img(opt.input)
     model = GANonymizer(opt)
+    start = time.time()
     out = model(img)
+    print('[INFO] elapsed time :', time.time() - start)
     if opt.mode != 'debug':
         out.save(os.path.join(opt.log, 'output.png'))
 
