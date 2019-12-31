f83a13d72348bb708733809c655d8e0e0feee4ce
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-01-01 01:33:46 +0900

    Fix shadow and resolution bugs

--- modules/mask_creater.py
+++ modules/mask_creater.py
@@ -9,45 +9,56 @@ from .utils import labels
 
 
 class MaskCreater:
-    def __init__(self, opt):
+    def __init__(self, opt, debugger):
         self.shadow_angle = opt.shadow_angle
         self.kernel_ratio = opt.kernel_ratio
         self.shadow_height_iter = opt.shadow_height_iter
         self.rough_obj_size = opt.rough_obj_size
         self.device = opt.device
 
-        self.target_ids = list()
+        self.debugger = debugger
+
+        self.prvobj_ids = list()
+        self.road_ids = list()
         for label in labels:
-            if ((label.category is 'vehicle' or label.category is 'human')
-                    and
-                    label.trainId is not 19):
-                self.target_ids.append(label.trainId)
+            if label.trainId == 19:
+                continue
+            if label.category in ['vehicle', 'human']:
+                self.prvobj_ids.append(label.trainId)
+            if label.category == 'flat':
+                self.road_ids.append(label.trainId)
 
     def __call__(self, segmap: torch.Tensor) -> (torch.Tensor, float):
-        mask = self.get_base_mask(segmap)
-        self.ksize = int(np.mean(mask.shape) * self.kernel_ratio)
-        mask = mask.reshape(1, 1, mask.shape[0], mask.shape[1])
-        mask = self.cleaning_mask(mask)
-        mask = self.include_shadow(mask)
-        max_obj_size = self.get_max_obj_size(mask)
-        return mask.squeeze().cpu(), max_obj_size
+        obj_mask = self.get_base_mask(segmap, self.prvobj_ids)
+        road_mask = self.get_base_mask(segmap, self.road_ids)
+        self.debugger.imsave(road_mask, 'road_mask.png')
+
+        self.set_base_kernel(segmap.shape)
+        obj_mask = obj_mask.reshape(1, 1, obj_mask.shape[0], obj_mask.shape[1])
+        obj_mask = self.cleaning_mask(obj_mask)
+        obj_mask = self.include_shadow(obj_mask, road_mask)
+        max_obj_size = self.get_max_obj_size(obj_mask)
+        return obj_mask.squeeze().cpu(), max_obj_size
+
+    def set_base_kernel(self, shape: tuple) -> None:
+        self.ksize = int(np.mean(shape) * self.kernel_ratio)
+        self.kernel = get_circle_kernel(self.ksize).to(self.device)
     
-    def get_base_mask(self, segmap: torch.Tensor) -> torch.Tensor:
+    def get_base_mask(self, segmap: torch.Tensor, label_ids: list) -> torch.Tensor:
         obj_mask = segmap.clone().to(self.device)
-        for id_ in self.target_ids:
+        road_mask = segmap.clone().to(self.device)
+        for id_ in label_ids:
             obj_mask = torch.where(obj_mask==id_,
                                    torch.full_like(obj_mask, 255),
                                    obj_mask)
         return where(obj_mask==255, 1, 0).to(torch.float32)
 
     def cleaning_mask(self, mask: torch.Tensor) -> torch.Tensor:
-        kernel = get_circle_kernel(self.ksize).to(self.device)
-        out = closing(mask, kernel)
-        out = opening(out, kernel)
+        out = closing(mask, self.kernel)
+        out = opening(out, self.kernel)
         return out
 
-    def include_shadow(self, mask: torch.Tensor) -> torch.Tensor:
-        size = int(np.mean(mask.shape[-2:]) * self.kernel_ratio)
+    def include_shadow(self, mask: torch.Tensor, road_mask: torch.Tensor) -> torch.Tensor:
         out = mask.clone()
         shadow_kernel = get_circle_kernel(
             self.ksize,
@@ -56,17 +67,17 @@ class MaskCreater:
         ).to(self.device)
         for _ in range(self.shadow_height_iter):
             out = morph_transform(out, shadow_kernel, transform='dilation')
-        return out
+        shadow = where(out - mask + road_mask == 2, 1, 0)
+        out = closing(mask + shadow, self.kernel)
+        return out.to(torch.float32)
 
     def get_max_obj_size(self, mask: torch.Tensor) -> int:
-        size = int(np.mean(mask.shape[-2:]) * self.kernel_ratio)
-        kernel = get_circle_kernel(self.ksize).to(self.device)
         out = mask.clone()
         for itr in range(100):
-            out = morph_transform(out, kernel, transform='erosion')
+            out = morph_transform(out, self.kernel, transform='erosion')
             if out.sum() == 0:
                 break
-        return itr * size
+        return itr * self.ksize
 
 
 def opening(img: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
