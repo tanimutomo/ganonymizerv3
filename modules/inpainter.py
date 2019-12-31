170df16e66ee913df767836535e900c6d726ab7c
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-01-01 01:32:55 +0900

    Add a replace function to inpaint module

--- modules/inpainter.py
+++ modules/inpainter.py
@@ -8,16 +8,21 @@ from .edge_connect.src.edge_connect import SimpleEdgeConnect
 
 
 class ImageInpainter():
-    def __init__(self, opt):
+    def __init__(self, opt, debugger):
         self.max_hole_size = opt.max_hole_size
+        self.debugger = debugger
+
         if opt.inpaint_method == 'EdgeConnect':
             self.model = self.set_edge_connect(opt)
 
     def __call__(self, img: torch.Tensor, mask: torch.Tensor, max_obj_size: float):
         resize_factor = min(self.max_hole_size / max_obj_size, 1)
-        img = self.resize(img, resize_factor)
-        mask = self.resize(mask.unsqueeze(0), resize_factor).squeeze()
-        return self.model.inpaint(img, mask)
+        img_resized = self.resize(img, resize_factor)
+        mask_resized = self.resize(mask.unsqueeze(0), resize_factor).squeeze()
+        inpainted_resized, inpainted_edge, edge = self.model.inpaint(img_resized, mask_resized)
+        print('[INFO] inpainted image size :', inpainted_resized.shape)
+        inpainted = self.replace_original(img, mask, inpainted_resized)
+        return inpainted, inpainted_edge, edge
 
     def resize(self, img: torch.Tensor, resize_factor: float) -> torch.Tensor:
         new_h = int(img.shape[1] * resize_factor)
@@ -27,10 +32,14 @@ class ImageInpainter():
         img = F.to_pil_image(img).resize((new_w, new_h))
         return F.to_tensor(img)
 
+    def replace_original(self, img: torch.Tensor, mask: torch.Tensor,
+                         inpainted_resized: torch.Tensor) -> torch.Tensor:
+        inpainted = F.to_pil_image(inpainted_resized).resize((img.shape[-1], img.shape[-2]))
+        return torch.where(mask == 1, F.to_tensor(inpainted), img)
+
     def set_edge_connect(self, opt):
         inpaint_ckpt = 'modules/edge_connect/checkpoints'
         model = SimpleEdgeConnect(inpaint_ckpt, 
                                   opt.sigma,
                                   opt.device)
         return model
-
