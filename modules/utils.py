c69bee2f7200ace9495a385f069df414401e0522
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-01-15 14:17:50 +0900

    Add colormap visualization function to DebugPrinter

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,7 @@
 import collections
 import cv2
+import matplotlib.pyplot as plt
+import mpl_toolkits
 import numpy as np
 import os
 import PIL
@@ -10,6 +12,7 @@ import torchvision
 
 from PIL import Image
 from collections import namedtuple
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from torchvision import transforms
 
 
@@ -55,7 +58,10 @@ class Debugger:
             return
         s = '[DEBUG] ' + comment + ' >>> '
         if type(mat) is torch.Tensor:
-            if 'float' in str(mat.dtype):
+            if 'bool' in str(mat.dtype):
+                s += 'shape: {}   dtype: {}   min: {}   max: {}   device: {}'.format(
+                    mat.shape, mat.dtype, mat.min(), mat.max(), mat.device)
+            elif 'float' in str(mat.dtype):
                 s += 'shape: {}   dtype: {}   min: {}   mean: {}   median: {}   max: {}   device: {}'.format(
                     mat.shape, mat.dtype, mat.min(), mat.mean(), mat.median(), mat.max(), mat.device)
             else:
@@ -74,17 +80,26 @@ class Debugger:
         print(s)
 
     def imsave(self, img, filename):
-        if self.mode != 'save':
-            return
+        if self.mode != 'save': return
         path = os.path.join(self.save_dir, filename)
         if type(img) is torch.Tensor:
             img = self.to_pil(img.to(torch.float32).squeeze().cpu())
-            img.save(path)
         elif type(img) is np.ndarray:
             img = Image.fromarray(img)
-            img.save(path)
         else:
             raise RuntimeError('The type of input image must be numpy.ndarray or torch.Tensor.')
+        img.save(path)
+
+    def save_colormap(self, tensor: torch.Tensor, filename: str) -> None:
+        if self.mode != 'save': return
+        path = os.path.join(self.save_dir, filename)
+        tensor = tensor.squeeze().cpu().numpy()
+        fig, ax = plt.subplots()
+        img = ax.imshow(tensor, cmap='jet')
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.10)
+        fig.colorbar(img, cax=cax)
+        plt.savefig(path)
 
     def exit(self):
         print('[DEBUG] code is exited by Debugger')
